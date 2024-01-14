faff4fa129db3ffcb290c269ad6821f83a43fa2e
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-01-15 00:15:58 +0100

    Add Manual Player Strategy with Promises

--- js/game.js
+++ js/game.js
@@ -1,22 +1,53 @@
 "use strict";
 
-// declare a class named Strategy
+// is this a correct way to declare a base class?
 class Strategy {
   constructor() {
     this.name = null;
   }
   getMove() {
-    return "stone";
+    return null;
   }
 }
 
+class RockStrategy extends Strategy {
+  constructor() {
+    super();
+    this.name = "Rock";
+  }
+  getMove() {
+    return "rock";
+  }
+}
+
+class PaperStrategy extends Strategy {
+  constructor() {
+    super();
+    this.name = "Paper";
+  }
+  getMove() {
+    return "paper";
+  }
+}
+
+class ScissorsStrategy extends Strategy {
+  constructor() {
+    super();
+    this.name = "Scissors";
+  }
+  getMove() {
+    return "scissors";
+  }
+}
+
+// is this a correct way to use inheritance?
 class RandomStrategy extends Strategy {
   constructor() {
     super();
     this.name = "Random";
   }
   getMove() {
-    const moves = ["stone", "paper", "scissors"];
+    const moves = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * 3);
     return moves[randomIndex];
   }
@@ -28,13 +59,17 @@ class Game {
     this.player2 = new RandomStrategy();
     this.player1Score = 0;
     this.player2Score = 0;
+    this.scoreDisplay = document.querySelector("#score");
     this.round = 0;
     this.maxRounds = 3;
+  }
+  setScoreDisplay() {
+    this.scoreDisplay.innerHTML = `${this.player1Score} : ${this.player2Score}`;
   }
   // declare a method named getMove
   async playRound() {
     this.round++;
-    const player1Move = this.player1.getMove();
+    const player1Move = await this.player1.getMove();
     const player2Move = this.player2.getMove();
 
     printMessage(`Round ${this.round}:`);
@@ -44,8 +79,8 @@ class Game {
     if (player1Move === player2Move) {
       printMessage("This round is a tie!");
     } else if (
-      (player1Move === "stone" && player2Move === "scissors") ||
-      (player1Move === "paper" && player2Move === "stone") ||
+      (player1Move === "rock" && player2Move === "scissors") ||
+      (player1Move === "paper" && player2Move === "rock") ||
       (player1Move === "scissors" && player2Move === "paper")
     ) {
       this.player1Score++;
@@ -54,11 +89,51 @@ class Game {
       this.player2Score++;
       printMessage("Player 2 wins the round!");
     }
+    this.setScoreDisplay();
   }
-  playGame() {
+
+  async playGame(maxRounds, player1Strategy, player2Strategy) {
+    this.maxRounds = maxRounds;
+
+    // choose strategy for player1
+    if (player1Strategy === "Random") {
+      this.player1 = new RandomStrategy();
+    } else if (player1Strategy === "AlwaysRock") {
+      this.player1 = new RockStrategy();
+    } else if (player1Strategy === "AlwaysPaper") {
+      this.player1 = new PaperStrategy();
+    } else if (player1Strategy === "AlwaysScissors") {
+      this.player1 = new ScissorsStrategy();
+    } else if (player1Strategy === "Ask") {
+      this.player1 = new AskForMoveStrategy();
+    } else {
+      this.player1 = new RandomStrategy();
+    }
+
+    // choose strategy for player2
+    if (player2Strategy === "Random") {
+      this.player2 = new RandomStrategy();
+    } else if (player2Strategy === "AlwaysRock") {
+      this.player2 = new RockStrategy();
+    } else if (player2Strategy === "AlwaysPaper") {
+      this.player2 = new PaperStrategy();
+    } else if (player2Strategy === "AlwaysScissors") {
+      this.player2 = new ScissorsStrategy();
+    } else if (player2Strategy === "Ask") {
+      this.player2 = new AskForMoveStrategy();
+    } else {
+      this.player2 = new RandomStrategy();
+    }
+
+    this.player1Score = 0;
+    this.player2Score = 0;
+    this.round = 0;
+    this.maxRounds = 3;
+    this.setScoreDisplay();
+
     clearMessages();
     while (this.round < this.maxRounds) {
-      this.playRound();
+      await this.playRound();
     }
     printMessage("Game over!");
     if (this.player1Score > this.player2Score) {
@@ -71,25 +146,44 @@ class Game {
   }
 }
 
-// class AskForMoveStrategy extends Strategy {
-//   constructor() {
-//     super();
-//     this.name = "AskForMove";
-//   }
-//   askForMove() {
-//     const move = prompt("Enter your move: stone, paper or scissors");
-//     return move;
-//   }
+function waitForEvent(elements, event) {
+  let handlers = [];
+  return new Promise((resolve) => {
+    elements.forEach((element, index) => {
+      handlers[index] = function (e) {
+        console.log(`Event ${event} happened on ${element.innerHTML}`);
+        elements.forEach((element, i) => {
+          element.removeEventListener(event, handlers[i]);
+        });
+        resolve(element.id);
+      };
+      element.addEventListener(event, handlers[index]);
+    });
+  });
+}
 
-//   getMove() {
-//     this.move = null;
-//     this.move = this.askForMove();
+class AskForMoveStrategy extends Strategy {
+  constructor() {
+    super();
+    this.name = "AskForMove";
+    this.rockButton = document.querySelector("#rockButton1");
+    this.paperButton = document.querySelector("#paperButton1");
+    this.scissorsButton = document.querySelector("#scissorsButton1");
+    // this.rockButton.addEventListener('click', function () {})
+  }
 
-//     while (move !== "stone" && move !== "paper" && move !== "scissors") {
-//       printMessage(`${move} is an invalid move! Try again.`);
-//       console.log(`${move} is an invalid move! Try again.`);
-//       setTimeout(this.askForMove(), 1000);
-//     }
-//     return this.move;
-//   }
-// }
+  async getMove() {
+    const id = await waitForEvent(
+      [this.rockButton, this.paperButton, this.scissorsButton],
+      "click"
+    );
+    if (id === "rockButton1") {
+      return "rock";
+    } else if (id === "paperButton1") {
+      return "paper";
+    } else if (id === "scissorsButton1") {
+      return "scissors";
+    }
+    return;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,53 @@
 "use strict";
 
+// add event listeners to buttons
+document
+  .getElementById("startGameButton")
+  .addEventListener("click", function () {
+    //read the value of the input field and assign it to the variable
+
+    game = new Game();
+    let numberOfRounds = Number(
+      document.getElementById("numberOfRounds").value
+    );
+    if (
+      numberOfRounds === 0 ||
+      numberOfRounds === null ||
+      numberOfRounds === undefined
+    ) {
+      numberOfRounds = 3;
+    }
+
+    let strategy1 = document.getElementById("player1Strategy").value;
+    let strategy2 = document.getElementById("player2Strategy").value;
+    if (strategy1 === "Ask") {
+      document.querySelector("#rockButton1").disabled = false;
+      document.querySelector("#paperButton1").disabled = false;
+      document.querySelector("#scissorsButton1").disabled = false;
+    } else {
+      document.querySelector("#rockButton1").disabled = true;
+      document.querySelector("#paperButton1").disabled = true;
+      document.querySelector("#scissorsButton1").disabled = true;
+    }
+
+    game.playGame(numberOfRounds, strategy1, strategy2);
+    document.getElementById("startGameButton").disabled = true;
+    document.getElementById("resetGameButton").disabled = false;
+  });
+
+document
+  .getElementById("resetGameButton")
+  .addEventListener("click", function () {
+    //read the value of the input field and assign it to the variable
+    clearMessages();
+    document.querySelector("#rockButton1").disabled = true;
+    document.querySelector("#paperButton1").disabled = true;
+    document.querySelector("#scissorsButton1").disabled = true;
+    game = null;
+    game = new Game();
+    document.getElementById("startGameButton").disabled = false;
+    document.getElementById("resetGameButton").disabled = true;
+  });
+
 let game = new Game();
-game.playGame();
+// game.playGame();
