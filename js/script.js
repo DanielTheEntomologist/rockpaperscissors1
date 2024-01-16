d997167fca81a80bcc5f08f8c8c121a0642a54dc
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-01-16 21:31:20 +0100

    Get Player Move

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ let moves = ["kamień", "papier", "nożyce"];
 // get random number between 0 and 2
 let randomNumber = Math.floor(Math.random() * 3);
 let computerMove = moves[randomNumber];
+
+let playerMove = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+
+if (playerMove == "1") {
+  playerMove = "kamień";
+} else if (playerMove == "2") {
+  playerMove = "papier";
+} else if (playerMove == "3") {
+  playerMove = "nożyce";
+} else {
+  playerMove = "nieznany ruch";
+}
+
+printMessage(`Twój ruch to: ${playerMove}`);
+printMessage("Mój ruch to: " + computerMove);
