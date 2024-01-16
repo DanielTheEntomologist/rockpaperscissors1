219799cf069998851544266a54721fe04058c650
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-01-16 22:04:37 +0100

    Move game logic into playGame, connect playGame to buttons

--- js/script.js
+++ js/script.js
@@ -1,34 +1,69 @@
 "use strict";
 
-let moves = ["kamień", "papier", "nożyce"];
-
-// get random number between 0 and 2
-let randomNumber = Math.floor(Math.random() * 3);
-let computerMove = moves[randomNumber];
-
-let playerMove = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-
-if (playerMove == "1") {
-  playerMove = "kamień";
-} else if (playerMove == "2") {
-  playerMove = "papier";
-} else if (playerMove == "3") {
-  playerMove = "nożyce";
-} else {
-  playerMove = "nieznany ruch";
+function moveToString(moveNumber) {
+  let moveName = "";
+  if (moveNumber == "1" || moveNumber == 1) {
+    moveName = "kamień";
+  } else if (moveNumber == "2" || moveNumber == 2) {
+    moveName = "papier";
+  } else if (moveNumber == "3" || moveNumber == 3) {
+    moveName = "nożyce";
+  } else {
+    moveName = "nieznany ruch";
+  }
+  return moveName;
 }
 
-printMessage(`Twój ruch to: ${playerMove}`);
-printMessage("Mój ruch to: " + computerMove);
-
-if (computerMove == playerMove) {
-  printMessage("Remis!");
-} else if (computerMove == "kamień" && playerMove == "nożyce") {
-  printMessage("Ja wygrywam!");
-} else if (computerMove == "papier" && playerMove == "kamień") {
-  printMessage("Ja wygrywam!");
-} else if (computerMove == "nożyce" && playerMove == "papier") {
-  printMessage("Ja wygrywam!");
-} else {
-  printMessage("Ty wygrywasz!");
+function determineWinner(computerMove, playerMove) {
+  if (computerMove == playerMove) {
+    return "Tie";
+  } else if (computerMove == "kamień" && playerMove == "nożyce") {
+    return "ComputerWin";
+  } else if (computerMove == "papier" && playerMove == "kamień") {
+    return "ComputerWin";
+  } else if (computerMove == "nożyce" && playerMove == "papier") {
+    return "ComputerWin";
+  } else {
+    return "PlayerWin";
+  }
 }
+
+function displayResult(computerMove, playerMove, winner) {
+  printMessage("Zagrałem " + computerMove + ", a Ty " + playerMove);
+  if (winner == "Tie") {
+    printMessage("Remis!");
+  } else if (winner == "ComputerWin") {
+    printMessage("Ja wygrywam!");
+  } else if (winner == "PlayerWin") {
+    printMessage("Ty wygrywasz!");
+  } else {
+    printMessage("Błąd!");
+  }
+}
+
+function playGame(playerInput) {
+  clearMessages();
+
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log("Wylosowana liczba to: " + randomNumber);
+
+  const computerMove = moveToString(randomNumber);
+  console.log("Ruch komputera to: " + computerMove);
+  const playerMove = moveToString(playerInput);
+  console.log("Twój ruch to: " + playerMove);
+
+  const winner = determineWinner(computerMove, playerMove);
+  console.log("Wynik to: " + winner);
+
+  displayResult(computerMove, playerMove, winner);
+}
+
+document.getElementById("play-rock").addEventListener("click", function () {
+  playGame(1);
+});
+document.getElementById("play-paper").addEventListener("click", function () {
+  playGame(2);
+});
+document.getElementById("play-scissors").addEventListener("click", function () {
+  playGame(3);
+});
