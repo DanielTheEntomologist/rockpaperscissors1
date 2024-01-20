32b674290f7bad08eef6f80095946ec10fc407b1
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-01-20 19:04:56 +0100

    Make all functions anonymous and minimise scope of variables

--- js/script.js
+++ js/script.js
@@ -1,69 +1,76 @@
 "use strict";
+const main = function () {
+  const moveToString = function (moveNumber) {
+    let moveName = "";
+    if (moveNumber == "1" || moveNumber == 1) {
+      moveName = "kamień";
+    } else if (moveNumber == "2" || moveNumber == 2) {
+      moveName = "papier";
+    } else if (moveNumber == "3" || moveNumber == 3) {
+      moveName = "nożyce";
+    } else {
+      moveName = "nieznany ruch";
+    }
+    return moveName;
+  };
 
-function moveToString(moveNumber) {
-  let moveName = "";
-  if (moveNumber == "1" || moveNumber == 1) {
-    moveName = "kamień";
-  } else if (moveNumber == "2" || moveNumber == 2) {
-    moveName = "papier";
-  } else if (moveNumber == "3" || moveNumber == 3) {
-    moveName = "nożyce";
-  } else {
-    moveName = "nieznany ruch";
-  }
-  return moveName;
-}
+  const determineWinner = function (computerMove, playerMove) {
+    if (computerMove == playerMove) {
+      return "Tie";
+    } else if (computerMove == "kamień" && playerMove == "nożyce") {
+      return "ComputerWin";
+    } else if (computerMove == "papier" && playerMove == "kamień") {
+      return "ComputerWin";
+    } else if (computerMove == "nożyce" && playerMove == "papier") {
+      return "ComputerWin";
+    } else {
+      return "PlayerWin";
+    }
+  };
 
-function determineWinner(computerMove, playerMove) {
-  if (computerMove == playerMove) {
-    return "Tie";
-  } else if (computerMove == "kamień" && playerMove == "nożyce") {
-    return "ComputerWin";
-  } else if (computerMove == "papier" && playerMove == "kamień") {
-    return "ComputerWin";
-  } else if (computerMove == "nożyce" && playerMove == "papier") {
-    return "ComputerWin";
-  } else {
-    return "PlayerWin";
-  }
-}
+  const displayResult = function (computerMove, playerMove, winner) {
+    printMessage("Zagrałem " + computerMove + ", a Ty " + playerMove);
+    if (winner == "Tie") {
+      printMessage("Remis!");
+    } else if (winner == "ComputerWin") {
+      printMessage("Ja wygrywam!");
+    } else if (winner == "PlayerWin") {
+      printMessage("Ty wygrywasz!");
+    } else {
+      printMessage("Błąd!");
+    }
+  };
 
-function displayResult(computerMove, playerMove, winner) {
-  printMessage("Zagrałem " + computerMove + ", a Ty " + playerMove);
-  if (winner == "Tie") {
-    printMessage("Remis!");
-  } else if (winner == "ComputerWin") {
-    printMessage("Ja wygrywam!");
-  } else if (winner == "PlayerWin") {
-    printMessage("Ty wygrywasz!");
-  } else {
-    printMessage("Błąd!");
-  }
-}
+  const playGame = function (playerInput) {
+    clearMessages();
 
-function playGame(playerInput) {
-  clearMessages();
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log("Wylosowana liczba to: " + randomNumber);
 
-  const randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log("Wylosowana liczba to: " + randomNumber);
+    const computerMove = moveToString(randomNumber);
+    console.log("Ruch komputera to: " + computerMove);
+    const playerMove = moveToString(playerInput);
+    console.log("Twój ruch to: " + playerMove);
 
-  const computerMove = moveToString(randomNumber);
-  console.log("Ruch komputera to: " + computerMove);
-  const playerMove = moveToString(playerInput);
-  console.log("Twój ruch to: " + playerMove);
+    const winner = determineWinner(computerMove, playerMove);
+    console.log("Wynik to: " + winner);
 
-  const winner = determineWinner(computerMove, playerMove);
-  console.log("Wynik to: " + winner);
+    displayResult(computerMove, playerMove, winner);
+  };
 
-  displayResult(computerMove, playerMove, winner);
-}
+  const rockButton = document.getElementById("play-rock");
+  const paperButton = document.getElementById("play-paper");
+  const scissorsButton = document.getElementById("play-scissors");
 
-document.getElementById("play-rock").addEventListener("click", function () {
-  playGame(1);
-});
-document.getElementById("play-paper").addEventListener("click", function () {
-  playGame(2);
-});
-document.getElementById("play-scissors").addEventListener("click", function () {
-  playGame(3);
-});
+  rockButton.addEventListener("click", function () {
+    playGame(1);
+  });
+  paperButton.addEventListener("click", function () {
+    playGame(2);
+  });
+  scissorsButton.addEventListener("click", function () {
+    playGame(3);
+  });
+};
+
+main();
